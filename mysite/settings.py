e87ffc5f5d93d423f7fb451e350aa82aeb0fc060
Ria Sehgal <51851121+Ria9929@users.noreply.github.com>
2021-04-23 18:54:34 +0530

    Deleted SECRET_KEY

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '86fs!1xsg^8m#=@y@5v6t8h$_bj$jvo8u_dw1v+0-_hv6&0wbv'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
